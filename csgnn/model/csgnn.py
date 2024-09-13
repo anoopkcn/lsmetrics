a0b9255537c1053bbbe196f7e6c4ae829d8ef33c
Anoop K. Chandran <a.chandran@fz-juelich.de>
2024-09-13 09:37:18 +0200

    change convelution back to CSGconv

--- csgnn/model/csgnn.py
+++ csgnn/model/csgnn.py
@@ -1,46 +1,39 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv, global_mean_pool
+from torch_geometric.nn import CGConv, global_mean_pool
 
 class CSGNN(torch.nn.Module):
-    def __init__(self, num_node_features, num_edge_features, hidden_channels, num_layers, dropout_rate=0.5):
+    def __init__(self, num_node_features, num_edge_features, hidden_channels, num_layers):
         super(CSGNN, self).__init__()
         self.num_layers = num_layers
 
         # Initial node embedding
         self.node_embedding = nn.Linear(num_node_features, hidden_channels)
 
-        # Graph Convolutional layers
+        # CGConv layers
         self.convs = nn.ModuleList()
         for _ in range(num_layers):
-            self.convs.append(GCNConv(hidden_channels, hidden_channels))
-
-        # Edge embedding
-        self.edge_embedding = nn.Sequential(
-            nn.Linear(num_edge_features, hidden_channels),
-            nn.ReLU(),
-            nn.Linear(hidden_channels, hidden_channels)
-        )
+            self.convs.append(CGConv(hidden_channels, num_edge_features, bias=True))
 
         # Output layers
         self.linear1 = nn.Linear(hidden_channels, hidden_channels)
         self.linear2 = nn.Linear(hidden_channels, 1)  # For property prediction
 
-        self.dropout = nn.Dropout(dropout_rate)
+        # Batch normalization layers (optional)
+        self.batch_norms = nn.ModuleList([nn.BatchNorm1d(hidden_channels) for _ in range(num_layers)])
 
     def forward(self, data, mode='regression'):
         x, edge_index, edge_attr, batch = data.x, data.edge_index, data.edge_attr, data.batch
 
-        # Initial node and edge embeddings
+        # Initial node embedding
         x = self.node_embedding(x)
-        edge_attr = self.edge_embedding(edge_attr)
 
         # Graph convolutions
-        for conv in self.convs:
+        for i, conv in enumerate(self.convs):
             x = conv(x, edge_index, edge_attr)
             x = F.relu(x)
-            x = self.dropout(x)
+            x = self.batch_norms[i](x)  # Apply batch normalization instead of dropout
 
         # Global pooling
         graph_embedding = global_mean_pool(x, batch)
@@ -51,8 +44,7 @@ class CSGNN(torch.nn.Module):
             # Final layers for property prediction
             x = self.linear1(graph_embedding)
             x = F.relu(x)
-            x = self.dropout(x)
-            property_prediction = self.linear2(x)
+            property_prediction = self.linear2(x).squeeze()
             return property_prediction
         else:
             raise ValueError("Invalid mode. Use 'encoder' or 'regression'.")
