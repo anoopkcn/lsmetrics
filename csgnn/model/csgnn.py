1f341f8b2322aeff6b8284389bcb4cdac19ce221
Anoop K. Chandran <a.chandran@fz-juelich.de>
2024-09-15 16:28:22 +0200

    add attention based convolution model

--- csgnn/model/csgnn.py
+++ csgnn/model/csgnn.py
@@ -3,14 +3,22 @@ import torch.nn as nn
 from torch.optim.adam import Adam
 import torch.nn.functional as F
 from torch_geometric.nn import CGConv, global_mean_pool
+from torch_geometric.nn import GATConv
 import pytorch_lightning as pl
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 
-class CSGNN(pl.LightningModule):
+class CSGCNN(pl.LightningModule):
     def __init__(
-        self, num_node_features, num_edge_features, hidden_channels, num_layers
+        self,
+        num_node_features,
+        num_edge_features,
+        hidden_channels,
+        num_layers,
+        learning_rate=0.01,
     ):
         super().__init__()
+        self.learning_rate = learning_rate
         self.num_layers = num_layers
 
         # Initial node embedding
@@ -18,17 +26,16 @@ class CSGNN(pl.LightningModule):
 
         # CGConv layers
         self.convs = nn.ModuleList()
+        self.batch_norms = nn.ModuleList()
         for _ in range(num_layers):
             self.convs.append(CGConv(hidden_channels, num_edge_features, bias=True))
+            self.batch_norms.append(
+                nn.BatchNorm1d(hidden_channels, track_running_stats=False)
+            )
 
         # Output layers
         self.linear1 = nn.Linear(hidden_channels, hidden_channels)
         self.linear2 = nn.Linear(hidden_channels, 1)  # For property prediction
-
-        # Batch normalization layers (optional)
-        self.batch_norms = nn.ModuleList(
-            [nn.BatchNorm1d(hidden_channels) for _ in range(num_layers)]
-        )
 
     def forward(self, data, mode="regression"):
         x, edge_index, edge_attr, batch = (
@@ -45,7 +52,8 @@ class CSGNN(pl.LightningModule):
         for i, conv in enumerate(self.convs):
             x = conv(x, edge_index, edge_attr)
             x = F.relu(x)
-            x = self.batch_norms[i](x)  # Apply batch normalization instead of dropout
+            if x.size(0) > 1:
+                x = self.batch_norms[i](x)
 
         # Global pooling
         graph_embedding = global_mean_pool(x, batch)
@@ -79,4 +87,86 @@ class CSGNN(pl.LightningModule):
         self.log("val_loss", loss, batch_size=batch.num_graphs, prog_bar=True)
 
     def configure_optimizers(self):
-        return Adam(self.parameters(), lr=0.01)
+        optimizer = Adam(self.parameters(), lr=self.learning_rate, weight_decay=1e-5)
+        scheduler = ReduceLROnPlateau(optimizer, mode="min", factor=0.5, patience=5)
+        return [optimizer], [scheduler]
+
+
+class CSGANN(pl.LightningModule):
+    def __init__(
+        self,
+        num_node_features,
+        num_edge_features,
+        hidden_channels,
+        num_layers,
+        learning_rate=0.01,
+    ):
+        super().__init__()
+        self.learning_rate = learning_rate
+        self.num_layers = num_layers
+        self.node_embedding = nn.Linear(num_node_features, hidden_channels)
+
+        self.convs = nn.ModuleList()
+        for _ in range(num_layers):
+            self.convs.append(
+                GATConv(hidden_channels, hidden_channels, edge_dim=num_edge_features)
+            )
+
+        self.layer_norms = nn.ModuleList(
+            [nn.LayerNorm(hidden_channels) for _ in range(num_layers)]
+        )
+        self.dropout = nn.Dropout(0.2)
+
+        self.linear1 = nn.Linear(hidden_channels, hidden_channels)
+        self.linear2 = nn.Linear(hidden_channels, 1)
+
+    def forward(self, data):
+        x, edge_index, edge_attr, batch = (
+            data.x,
+            data.edge_index,
+            data.edge_attr,
+            data.batch,
+        )
+        x = self.node_embedding(x)
+
+        for i, conv in enumerate(self.convs):
+            x = conv(x, edge_index, edge_attr)
+            x = F.elu(x)
+            x = self.layer_norms[i](x)
+            # x = self.dropout(x)
+
+        x = global_mean_pool(x, batch)
+        return x
+
+    def encode(self, data):
+        """
+        Encode the input data into a graph embedding.
+        """
+        return self.forward(data)
+
+    def predict_property(self, data):
+        """
+        Predict the property using the graph embedding.
+        """
+        x = self.encode(data)
+        x = self.linear1(x)
+        x = F.elu(x)
+        # x = self.dropout(x)
+        x = self.linear2(x).view(-1)
+        return x
+
+    def training_step(self, batch, batch_idx):
+        y_hat = self.predict_property(batch)
+        loss = F.huber_loss(y_hat, batch.y.view(-1))
+        self.log("train_loss", loss, batch_size=batch.num_graphs, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        y_hat = self.predict_property(batch)
+        loss = F.huber_loss(y_hat, batch.y.view(-1))
+        self.log("val_loss", loss, batch_size=batch.num_graphs, prog_bar=True)
+
+    def configure_optimizers(self):
+        optimizer = Adam(self.parameters(), lr=self.learning_rate, weight_decay=1e-5)
+        scheduler = ReduceLROnPlateau(optimizer, mode="min", factor=0.5, patience=5)
+        return [optimizer], [scheduler]
